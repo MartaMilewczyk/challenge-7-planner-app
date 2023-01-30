6bd9f8a47543272fdc50029c04cf5c60bffae7d3
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-01-30 18:31:45 +0100

    rendering of calendar rows added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 const currentDayEl = $('#currentDay');
+const calendarEventEl = $('#calendar-event');
+const calendarContainerEl = $('#calendar-container');
+const eventHourEl = $('#event-hour');
+const eventTextEl = $('#event-text');
+const eventSaveEl = $('#event-save');
 
+let startHour = 9;
+let endHour = 17;
 
 // time display
 function todayDateDisplay() {
@@ -7,6 +14,15 @@ function todayDateDisplay() {
     currentDayEl.text(todayDate);
 }
 
-
-
 setInterval(todayDateDisplay, 1);
+
+function renderCalendarRows() {
+    // calendarEventEl.hide(':first-child');
+
+    for (let i = startHour; i <= endHour; i++) {
+        calendarEventEl.clone().appendTo(calendarContainerEl);
+        i < 12 ? eventHourEl.text(i + ":00 am") : eventHourEl.text(i + ":00 pm")
+    }
+}
+
+renderCalendarRows();
