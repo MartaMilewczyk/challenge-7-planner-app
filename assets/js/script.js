94792270e9a50a0ce99c103e5a4ea4dd5e170742
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-01-30 20:10:41 +0100

    classes added with reference to current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ let endHour = 17;
 
 // time display
 function todayDateDisplay() {
-    let todayDate = dayjs(). format('dddd[,] DD MMM YYYY [at] hh:mm:ss a');
+    let todayDate = dayjs(). format('dddd[,] DD MMM YYYY [at] HH:mm:ss');
     currentDayEl.text(todayDate);
 }
 
@@ -19,12 +19,20 @@ setInterval(todayDateDisplay, 1);
 function renderCalendarRows() {
     calendarEventEl.hide(':first-child');
     const calEventEl = $('.calendar-event');
+    let currentHour = dayjs().format('h');
 
     for (let i = startHour; i <= endHour; i++) {
         let newEl = calEventEl.clone().appendTo(calendarContainerEl);
         evHourEl = newEl.children('.event-hour');
-        i < 12 ? evHourEl.text(i + ":00 am") : evHourEl.text(i + ":00 pm")
+        evHourEl.text(i + ":00")
+
+        currentHour < i ? eventTextEl.addClass("future")
+        : currentHour === i ? eventTextEl.addClass("present")
+        : eventTextEl.addClass("past");
     }
+
+    console.log(evHourEl);
 }
 
 renderCalendarRows();
+
